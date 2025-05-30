b7c53072651fbb1ae4ce436f9906bba3d70e7314
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-05-30 12:10:57 +0200

    Create admin interface for managing booking data

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Location, Table, Reservation
 
-# Register your models here.
+# Register models
+admin.site.register(Location)
+admin.site.register(Table)
+admin.site.register(Reservation)
